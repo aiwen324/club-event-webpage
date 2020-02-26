f966ef4b2bd34589a322d1758d49ea83aef087e1
aiwen324 <aiwen324@gmail.com>
2020-02-25 23:16:11 -0500

    Add files

--- src/react-components/Home/index.js
+++ src/react-components/Home/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import './style.css';
-// import bgImage from './images/home-books.jpg';
+import Posts from '../Posts' 
+import { Link as RouterLink } from 'react-router-dom';
+import Link from '@material-ui/core/Link'
 
 class Home extends React.Component {
     render() {
@@ -10,7 +12,7 @@ class Home extends React.Component {
                 <div id="Navigation-bar">
                     <div id="accountInfo">
                         <a>Login</a>
-                    </div>  
+                    </div>
                     <ul>
                         <li><a>Home</a></li>
                         <li><a>Announcements</a></li>
@@ -18,17 +20,127 @@ class Home extends React.Component {
                         <li><a>About us</a></li>
                     </ul>
                 </div>
-                {/* <div className="center" id="Backgorund-img">
+                <div id="Backgorund-img">
+                    <div id="Welcome-section">
+                        <h1 id="Welcome">Welcome to UTACG survey center</h1>
+                        <p>Your opinion is valuable to us</p>
+                    </div>
+                </div>
+                <div className='posts'>
+                    <Link underline='none' component={RouterLink} to='/your-target-path'>
+                        <Posts/>
+                    </Link>
+                    <Link underline='none' component={RouterLink} to='/your-target-path'>
+                        <Posts/>
+                    </Link>
+                    <Link underline='none' component={RouterLink} to='/your-target-path'>
+                        <Posts/>
+                    </Link>
+                    <Link underline='none' component={RouterLink} to='/your-target-path'>
+                        <Posts/>
+                    </Link>
+                </div>
+                {/* <div id='Backgorund-img'>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
+                    <div className="event-forms">
+                        <p>hahahh</p>
+                    </div>
                 </div> */}
-                <div id="Welcome-section">
-                    <h1 id="Welcome">Welcome to UTACG survey center</h1>
-                    <p>Your opinion is valuable to us</p>
-                </div>
-                
-                <div id="event-forms">
-                    <p>hahahh</p>
-                </div>
-                <img className='center' id='Backgorund-img' alt='Error'/>
             </div>
         )
     }
---
+++ src/react-components/Posts/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './style.css';
+
+class Posts extends React.Component {
+
+    render() {
+        return (
+            <div className='post'>
+                <h3>Announcement 1</h3>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Quisque id tristique libero, sit amet tempus ex. In egestas
+                enim felis, vel dapibus dolor malesuada vitae. Praesent
+                convallis massa lectus, at
+            </div>
+        )
+    }
+}
+
+export default Posts;
