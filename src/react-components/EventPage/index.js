ec6d94c50cc7f7bb57234ea64fdb90b7693f8c8d
shoko-inori <frank.gong.1006@hotmail.com>
2020-02-27 17:21:00 -0500

    Some styling

--- src/react-components/EventPage/index.js
+++ src/react-components/EventPage/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './style.css';
 // import Posts from '../Posts' 
-// import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 // import Link from '@material-ui/core/Link'
 
 class EventPage extends React.Component {
@@ -19,6 +19,13 @@ class EventPage extends React.Component {
                         <li><a>Events</a></li>
                         <li><a>About us</a></li>
                     </ul>
+                </div>
+                <div>
+                    <h1 id='event_title'>Project Demo</h1>
+                </div>
+                <div id='event_detail'>
+                    <h3 id='event_time'>Date and Time: <p>Friday, February 28, 2020 @ 14:40</p></h3>
+                    <h3 id='event_loc'>Location: <p>Myhal Centre, Rm. 317</p></h3>
                 </div>
             </div>
         )
