812eb4f799b223dda146b8e6946b7c2467e8efdb
shoko-inori <frank.gong.1006@hotmail.com>
2020-02-27 22:15:32 -0500

    Some work on the survey question

---
+++ src/react-components/SurveyQuestion/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './style.css';
+
+class SurveyQuestion extends React.Component {
+    render() { 
+        return (
+            <div className="survey_question">
+                <div id='survey_question'>
+                    This is the question. 
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SurveyQuestion
