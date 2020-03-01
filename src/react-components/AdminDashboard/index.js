77901574e6c8742235c54ddbf612a81e6274118a
shoko-inori <frank.gong.1006@hotmail.com>
2020-03-01 18:47:26 -0500

    Creation of the admin dashboard component

---
+++ src/react-components/AdminDashboard/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './style.css'
+
+class AdminDashboard extends React.Component {
+    render() {
+        return (
+            <div id='admin_dashboard'>
+
+            </div>
+        )
+    }
+
+}
+
+export default AdminDashboard
