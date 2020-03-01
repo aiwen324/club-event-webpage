ad9ba70beddaa48027fc754ff42560d8a63d3f97
shoko-inori <frank.gong.1006@hotmail.com>
2020-03-01 17:05:55 -0500

    Application of the new design on the survey page

--- src/react-components/SurveyPage/index.js
+++ src/react-components/SurveyPage/index.js
@@ -13,46 +13,50 @@ import { Button } from '@material-ui/core'
 class SurveyPage extends React.Component {
     render() {
         return (
-            <div className="event_page">
-                <div>
-                    <h1 id='event_title'>Which anime are you watching this season?</h1>
-                </div>
-                <div id='event_details'>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-                        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
-                        sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
-                        est laborum.
-                    </p>
-                </div>
-                <div>
-                    <img className="event_image" src={require("./images/ao-manaka-1.jpeg")}/>
-                </div>
-                <div>
-                    <img className="event_image" src={require("./images/ao-manaka-2.jpeg")}/>
-                </div>
-                {/* <div id='survey_form'>
+            <div id='event_page_contents'>
+                <div className="event_page">
+                    <div>
+                        <h1 id='event_title'>Which anime are you watching this season?</h1>
+                    </div>
+                    <div id='event_details'>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                            ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur 
+                            sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id 
+                            est laborum.
+                        </p>
+                    </div>
+                    <div>
+                        <img className="event_image" src={require("./images/ao-manaka-1.jpeg")}/>
+                    </div>
+                    <div>
+                        <img className="event_image" src={require("./images/ao-manaka-2.jpeg")}/>
+                    </div>
+                    {/* <div id='survey_form'>
+                        <SurveyQuestion/>
+                    </div> */}
+                    <div>
+                        <h2 className='event_section_title'>Survey Questions</h2>
+                    </div>
                     <SurveyQuestion/>
-                </div> */}
-                <div>
-                    <h2 className='event_section_title'>Survey Questions</h2>
+                    <SurveyQuestion/>
+                    <SurveyQuestion/>
+                    <FreeResponseQuestion/>
+                    <div id='submit_button'>
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            color="primary">
+                                Submit
+                        </Button>
+                    </div>
+                    <div className='bottom_padder'/>
                 </div>
-                <SurveyQuestion/>
-                <SurveyQuestion/>
-                <SurveyQuestion/>
-                <FreeResponseQuestion/>
-                <div id='submit_button'>
-                    <Button
-                        type="submit"
-                        variant="contained"
-                        color="primary">
-                            Submit
-                    </Button>
-                </div>
-                <div id='bottom_padder'/>
+                <div className='bottom_padder'/>
             </div>
+            
         )
     }
 }
