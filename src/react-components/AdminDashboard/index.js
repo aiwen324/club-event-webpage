68b59e7eb8e99f8cdf1fe4b3e6d28296201b830d
shoko-inori <frank.gong.1006@hotmail.com>
2020-03-02 01:54:40 -0500

    Code to remove navbar in the admin dashboard

--- src/react-components/AdminDashboard/index.js
+++ src/react-components/AdminDashboard/index.js
@@ -3,10 +3,17 @@ import React from 'react'
 import './style.css'
 
 class AdminDashboard extends React.Component {
+    componentDidMount() {
+        const nav_bar = document.getElementById('Navigation-bar')
+        if (nav_bar != null) {
+            nav_bar.parentNode.removeChild(nav_bar)
+        }
+    }
+
     render() {
         return (
             <div id='admin_dashboard'>
-
+                
             </div>
         )
     }
