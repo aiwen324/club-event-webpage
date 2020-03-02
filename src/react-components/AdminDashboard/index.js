c0835a035c04c45ee6e948c59e9a1106eee208c0
shoko-inori <frank.gong.1006@hotmail.com>
2020-03-02 14:46:22 -0500

    Greeting & its styling

--- src/react-components/AdminDashboard/index.js
+++ src/react-components/AdminDashboard/index.js
@@ -3,17 +3,37 @@ import React from 'react'
 import './style.css'
 
 class AdminDashboard extends React.Component {
+    state = {
+        greeting: null,
+    }
+
     componentDidMount() {
+        // Remove navbar
         const nav_bar = document.getElementById('Navigation-bar')
         if (nav_bar != null) {
             nav_bar.parentNode.removeChild(nav_bar)
         }
+
+        // Get greetings according to time of the day
+        const date = new Date()
+        const hour = date.getHours()
+        let greeting = null
+        if (hour < 12) {
+            greeting = 'Good morning'
+        } else if (hour >= 18) {
+            greeting = 'Good evening'
+        } else {
+            greeting = 'Good afternoon'
+        }
+        this.setState({ greeting })
+
     }
 
     render() {
+        const { greeting } = this.state
         return (
             <div id='admin_dashboard'>
-                
+                <h3 id='admin_greet'>{greeting}, Admin!</h3>
             </div>
         )
     }
