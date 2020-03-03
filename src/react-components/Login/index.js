1d51addf95bdeff444222dede8147bc2b5fcf3c7
aiwen324 <aiwen324@gmail.com>
2020-03-03 18:02:54 -0500

    Transform style to css file

--- src/react-components/Login/index.js
+++ src/react-components/Login/index.js
@@ -5,84 +5,77 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
 import { loginAuth } from '../../actions/login_auth'
-
 import './style.css';
-import { render } from '@testing-library/react';
 
-const useStyles = makeStyles(theme => ({
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
-}));
 
-export default function SignIn() {
-    const classes = useStyles();
+class SignIn extends React.Component {
+    // const classes = useStyles();
 
-    return (
-        <div id='login_page'>
-            <div id='top_padding'/>
-            <div className='Login_panel'>
-                <div>
-                    <h2>Sign In</h2>
-                </div>
-                <form className={classes.form} noValidate>
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        autoFocus
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                    <FormControlLabel
-                        control={<Checkbox value="remember" color="primary" />}
-                        label="Remember me"
-                    />
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                        href='/admin'
-                    >
-                        Sign In
+    render() {
+
+        return (
+            <div id='login_page'>
+                <div id='top_padding' />
+                <div className='Login_panel'>
+                    <div>
+                        <h2>Sign In</h2>
+                    </div>
+                    <form className='form' noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className='submit'
+                            href='/admin'
+                        >
+                            Sign In
                     </Button>
-                    <Grid container>
-                        <Grid item xs>
-                            <Link href="#" variant="body2">
-                                Forgot password?
+                        <Grid container>
+                            <Grid item xs={3}>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
                             </Link>
+                            </Grid>
+                            <Grid item xs={9}>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
                         </Grid>
-                        <Grid item>
-                            <Link href="#" variant="body2">
-                                {"Don't have an account? Sign Up"}
-                            </Link>
-                        </Grid>
-                    </Grid>
-                </form>
+                    </form>
+                </div>
             </div>
-        </div>
-        
-    );
+
+        )
+    }
 }
+
+export default SignIn;
