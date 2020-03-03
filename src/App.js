1ee7300f8318c7751af0c6a57e8d82a532db1707
shoko-inori <frank.gong.1006@hotmail.com>
2020-03-03 18:23:38 -0500

    Refactor the app to be a class to enable recording the state

--- src/App.js
+++ src/App.js
@@ -14,39 +14,36 @@ import AdminDashboard from './react-components/AdminDashboard'
 import AdminEventPage from './react-components/AdminEventPage/index.js'
 import AdminSurveyPage from './react-components/AdminSurvey/index.js'
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/' render={() => 
-                          (<Home/>)}/>
-          <Route exact path='/login/' render={() =>
-                          (<Login/>)}/>
-          <Route path='/postid=:id' render={() => (<Home/>)} />
-          <Route exact path='/event' render={() =>
-                          (<EventPage/>)
-          }/>
-          <Route exact path='/survey' render={() =>
-                          (<SurveyPage/>)
-          }/>
-          <Route exact path='/admin/edit_announce' render={
-            ()=>(<AdminEdit/>)
-          }/>
-          <Route exact path='/admin' render={() =>
-                          (<AdminDashboard/>)
-          }/>
-          <Route exact path='/adminEventPage' render={() =>
-                          (<AdminEventPage/>)
-          }/>
-          <Route exact path='/adminSurveyPage' render={() =>
-                          (<AdminSurveyPage/>)
-          }/>
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar/>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path='/' render={() => 
+                            (<Home/>)}/>
+            <Route exact path='/login/' render={() =>
+                            (<Login/>)}/>
+            <Route path='/postid=:id' render={() => (<Login/>)} />
+            <Route exact path='/event' render={() =>
+                            (<EventPage/>)
+            }/>
+            <Route exact path='/survey' render={() =>
+                            (<SurveyPage/>)
+            }/>
+            <Route exact path='/admin/edit_announce' render={
+              ()=>(<AdminEdit/>)
+            }/>
+            <Route exact path='/admin' render={() =>
+                            (<AdminDashboard/>)
+            }/>
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
