28705dd4546ccce234e5de9c0a25a4ab4d5d2e46
sheldonsu28 <sheldonsu28@gmail.com>
2020-03-26 21:59:02 -0400

    merge

--- src/server.js
+++ src/server.js
@@ -1,54 +1,54 @@
 /* server.js - user & resource authentication */
-'use strict';
-const log = console.log
+"use strict";
+const log = console.log;
 
-const express = require('express')
+const express = require("express");
 
 const app = express();
 
 // mongoose and mongo connection
-const { mongoose } = require('./db/mongoose')
-mongoose.set('useFindAndModify', false); 
+const { mongoose } = require("./db/mongoose");
+mongoose.set("useFindAndModify", false);
 
-const { Announcement } = require('./models/Announcement')
-const { Users } = require('./models/users')
+const { Announcement } = require("./models/Announcements");
+const { Users } = require("./models/users");
 
 // to validate object IDs
-const { ObjectID } = require('mongodb')
+const { ObjectID } = require("mongodb");
 
 // handlebars templating engine
-const hbs = require('hbs')
+const hbs = require("hbs");
 // Set express property 'view engine' to be 'hbs'
-app.set('view engine', 'hbs')
+app.set("view engine", "hbs");
 // setting up partials directory
-hbs.registerPartials(__dirname + '/views/partials')
-
+hbs.registerPartials(__dirname + "/views/partials");
 
 // body-parser: middleware for parsing HTTP JSON body into a usable object
-const bodyParser = require('body-parser') 
-app.use(bodyParser.json())
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 // express-session for managing user sessions
-const session = require('express-session')
+const session = require("express-session");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(session({
-    secret: '#66CCFF',
+app.use(
+  session({
+    secret: "#66CCFF",
     resave: false,
-    cookie:{
-        expires: 10000,
-        httppOnly: true
+    cookie: {
+      expires: 10000,
+      httppOnly: true
     }
-    
-}))
+  })
+);
 
-const validatelogin = (req, res, next)=>{
-    if(!req.session.Users){
-        res.redirct('/login/')
-    }else{
-        next()
-    }
-}
+const validatelogin = (req, res, next) => {
+  if (!req.session.Users) {
+    res.redirct("/login/");
+  } else {
+    next();
+  }
+};
 
 // API used to login, request json should be formated like:
 /*  {
@@ -66,30 +66,31 @@ const validatelogin = (req, res, next)=>{
 }
 */
 // If the login failed, it wil return a status code of 400
-app.post('/Login', (req, res)=>{
-    const username = req.body.email
-    const password = req.body.password
+app.post("/Login", (req, res) => {
+  const username = req.body.email;
+  const password = req.body.password;
 
-    Users.findOne({username: username, password: password}).then((result)=>{
-        if(!result){
-            res.status(400).send()
-        }
-        const returnJson = {
-            userID: result._id,
-            email: result.email,
-            username: result.username,
-            accountType:result.accountType,
-        }
-        req.session.userID = result._id
-        req.session.type= result.accountType
-        
-        res.status(200).json(returnJson)
-        
-    }, (error)=>{
-        res.status(500).send(error)
-    })
-})
+  Users.findOne({ username: username, password: password }).then(
+    result => {
+      if (!result) {
+        res.status(400).send();
+      }
+      const returnJson = {
+        userID: result._id,
+        email: result.email,
+        username: result.username,
+        accountType: result.accountType
+      };
+      req.session.userID = result._id;
+      req.session.type = result.accountType;
 
+      res.status(200).json(returnJson);
+    },
+    error => {
+      res.status(500).send(error);
+    }
+  );
+});
 
 // API for adding new users to server, expecting a json format like this:
 /*
@@ -103,55 +104,62 @@ app.post('/Login', (req, res)=>{
 */
 // Upon sucess, response have a status code of 200
 // Upon Failure, reponsecode is 500 and error detail will be send
-app.post('/signUp', (req, res)=>{
-    const newUser = new Users({
-        accountType: req.body.accountType,
-        username: req.body.username,
-        password: req.body.password,
-        phoneNumber: req.body.phoneNumber
-    })
-    newUser.save().then((result)=>(
-        res.status(200).send()
-    ), (error)=>{
-        res.status(500).send(error)
-    })
-})
-
+app.post("/signUp", (req, res) => {
+  const newUser = new Users({
+    accountType: req.body.accountType,
+    username: req.body.username,
+    password: req.body.password,
+    phoneNumber: req.body.phoneNumber
+  });
+  newUser.save().then(
+    result => res.status(200).send(),
+    error => {
+      res.status(500).send(error);
+    }
+  );
+});
 
 // This API is for getting all the visable announcement from the server
 //return {[results]}
-app.get('/visableAnnouncements',(req, res)=>{
-    //work in progress
-    Announcement.find({visable: 1}).then((result)=>{
-        res.status(200).send({result})
-    },(error)=>{
-        res.status(500).send(error)
-    });
-})
-
+app.get("/visableAnnouncements", (req, res) => {
+  //work in progress
+  Announcement.find({ visable: 1 }).then(
+    result => {
+      res.status(200).send({ result });
+    },
+    error => {
+      res.status(500).send(error);
+    }
+  );
+});
 
 // This API is for getting all the visable announcement
 //return {[results]}
-app.get('/getAllAnnouncement',(req, res)=>{
-    Announcement.find().then((result)=>{
-        res.send({result})
-    }, (error)=>{
-        res.status(500).send(error)
-    })
-})
-
+app.get("/getAllAnnouncement", (req, res) => {
+  Announcement.find().then(
+    result => {
+      res.send({ result });
+    },
+    error => {
+      res.status(500).send(error);
+    }
+  );
+});
 
 // Get Announcement by announcement id
 // return announcement object
-app.get('/Announcement/:id', (req, res)=>{
-    const announcementID = req.param.id
-    
-    Announcement.findById(announcementID).then((result)=>{
-        res.status(200).send(result)
-    },(error)=>{
-        res.status(500).send(error)
-    })
-})
+app.get("/Announcement/:id", (req, res) => {
+  const announcementID = req.param.id;
+
+  Announcement.findById(announcementID).then(
+    result => {
+      res.status(200).send(result);
+    },
+    error => {
+      res.status(500).send(error);
+    }
+  );
+});
 
 // Add comment to a Announcement
 /*
@@ -162,32 +170,42 @@ app.get('/Announcement/:id', (req, res)=>{
     }
 */
 // return a with updated post content
-app.post('/Announcement/:id', validatelogin,(req, res)=>{
-    const announcementID = req.param.id
+app.post("/Announcement/:id", validatelogin, (req, res) => {
+  const announcementID = req.param.id;
 
-    if(!ObjectID.isValid(announcementID)){
-        res.status(404).send()
+  if (!ObjectID.isValid(announcementID)) {
+    res.status(404).send();
+  }
+
+  const newComment = {
+    content: req.body.content,
+    userID: req.body.userID,
+    date: req.body.date
+  };
+
+  Announcement.findById(announcementID).then(
+    result => {
+      const comment = result.comments;
+      comment.push(newComment);
+
+      Announcement.findByIdAndUpdate(
+        announcementID,
+        { $set: { comments: Comment } },
+        { new: true }
+      ).then(
+        updateResult => {
+          res.status(200).json(updateResult);
+        },
+        error => {
+          res.status(500).send(error);
+        }
+      );
+    },
+    error => {
+      res.status(404).send(error);
     }
-
-    const newComment = {
-        content: req.body.content,
-        userID:  req.body.userID,
-        date:    req.body.date
-    }
-
-    Announcement.findById(announcementID).then((result)=>{
-        const comment = result.comments
-        comment.push(newComment)
-
-        Announcement.findByIdAndUpdate(announcementID, {$set: {comments: Comment}}, {new: true}).then((updateResult)=>{
-			res.status(200).json(updateResult)
-		}, (error)=>{
-			res.status(500).send(error)
-		})
-    },(error)=>{
-        res.status(404).send(error)
-    })
-})
+  );
+});
 
 // Register a user to activity, expect a json as follows:
 /*
@@ -195,7 +213,64 @@ app.post('/Announcement/:id', validatelogin,(req, res)=>{
         userID: <userID (Number)>
     }
 */
-//Upon sucess, return 200, if post does not exit, print failure.
-app.post('/Register/:id', validatelogin, (req, res)=>{
-    
-})
+//Upon sucess, return 200 and the updated announcement as json, if post does not exit, print failure.
+app.post("/Register/:id", validatelogin, (req, res) => {
+  const announcementID = req.param.id;
+  const userid = res.body.userID;
+
+  if (!ObjectID.isValid(announcementID)) {
+    res.status(404).send();
+  }
+
+  Users.findOne({ userID: userid }).then(
+    result => {
+      if (!result) {
+        res.status(404).send("User does not exitst");
+      }
+
+      Announcement.findById(announcementID).then(
+        result => {
+          if (!result) {
+            res.status(404).send("Announcement does not exit");
+          }
+
+          const userList = result.registeredUser;
+          userList.forEach(element => {
+            if (element === userid) {
+              res.status(500).send("User is already registerd");
+              return;
+            }
+          });
+          userList.push(userid);
+
+          Announcement.findByIdAndUpdate(
+            announcementID,
+            { $set: { registeredUser: userList } },
+            { new: true }
+          ).then(
+            result => {
+              res.status(200).json(result);
+            },
+            error => {
+              res.status(500).send(error);
+            }
+          );
+        },
+        error => {
+          res.status(500).send(error);
+        }
+      );
+    },
+    error => {
+      res.status(500).send(error);
+    }
+  );
+});
+
+// Update the announcement voting result
+/*
+    {
+        
+    }
+*/
+app.post("/updateVote", (req, res) => {});
