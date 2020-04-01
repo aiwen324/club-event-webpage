e4915ddf85daa0f3cd39bcc7656a232b2b070aab
aiwen324 <aiwen324@gmail.com>
2020-03-31 23:11:12 -0400

    Modify frontend to be compatible with future backend

--- src/react-components/Admin_edit/survey.js
+++ src/react-components/Admin_edit/survey.js
@@ -1,82 +1,96 @@
-import React from 'react'
-import { Grid } from '@material-ui/core';
-import IconButton from '@material-ui/core/IconButton';
-import DeleteIcon from '@material-ui/icons/Delete';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import TextField from '@material-ui/core/TextField'
-import './survey.css'
-
-
-
-
-
+import React from "react";
+import { Grid } from "@material-ui/core";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import TextField from "@material-ui/core/TextField";
+import "./survey.css";
 
 class Survey extends React.Component {
+  removeQuestion = (editPanel, index, e) => {
+    e.preventDefault();
+    const question_array = editPanel.state.question_array;
+    question_array.splice(index, 1);
+    editPanel.setState({ question_array: question_array });
+  };
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            index: 0,
-            type: 10,
-            options: ''
-        }
+  handleTypeSelection = (editPanel, index, event) => {
+    event.preventDefault();
+    const question_array = editPanel.state.question_array;
+    question_array[index].type = event.target.value;
+    if (event.target.value !== 0) {
+      question_array[index].options = "";
     }
+    editPanel.setState({ question_array: question_array });
+  };
 
-    handleChange2 = (event) => {
-        console.log(this.state)
-        console.log("Get event value", event.target.value)
-        this.setState({ type: event.target.value });
-    }
+  handleTextInput = (editPanel, index, e) => {
+    e.preventDefault();
+    const question_array = editPanel.state.question_array;
+    question_array[index][e.target.name] = e.target.value;
+    editPanel.setState({ question_array: question_array });
+  };
 
-
-    render() {
-
-        const { removeQuestion } = this.props;
-        return (
-            <div className='question_section'>
-                <Grid container spacing={2}>
-                    <Grid item xs={7}>
-                        <TextField
-                            className='question'
-                            label='Question'
-                            placeholder='Question'
-                            fullWidth
-                        />
-                    </Grid>
-                    <Grid item xs={2}>
-                        <FormControl>
-                            <Select value={this.state.type} onChange={this.handleChange2} displayEmpty>
-                                <MenuItem value={10}>Checkboxes</MenuItem>
-                                <MenuItem value={20}>Paragraph</MenuItem>
-                            </Select>
-                            <FormHelperText>Question Type</FormHelperText>
-                        </FormControl>
-                    </Grid>
-                    {
-                        this.state.type === 10 ?
-                            <Grid item xs={10}>
-                                <TextField
-                                    className='question'
-                                    label='options'
-                                    placeholder='Options sepereated by ; (e.g. a; b; c; d)'
-                                    fullWidth
-                                    multiline
-                                />
-                            </Grid> :
-                            <div></div>
-                    }
-                    <Grid item xs={2}>
-                        <IconButton aria-label="delete" onClick={removeQuestion(this)}>
-                            <DeleteIcon />
-                        </IconButton>
-                    </Grid>
-                </Grid>
-            </div>
-        )
-    }
+  render() {
+    const { editPanel, index, question } = this.props;
+    return (
+      <div className="question_section">
+        <Grid container spacing={2}>
+          <Grid item xs={7}>
+            <TextField
+              name="questionTitle"
+              className="question"
+              label="Question"
+              placeholder="Question"
+              value={editPanel.state.question_array[index].questionTitle}
+              onChange={e => this.handleTextInput(editPanel, index, e)}
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <FormControl>
+              <Select
+                value={question.type}
+                onChange={e => this.handleTypeSelection(editPanel, index, e)}
+                displayEmpty
+              >
+                <MenuItem value={0}>Checkboxes</MenuItem>
+                <MenuItem value={1}>Paragraph</MenuItem>
+              </Select>
+              <FormHelperText>Question Type</FormHelperText>
+            </FormControl>
+          </Grid>
+          {question.type === 0 ? (
+            <Grid item xs={10}>
+              <TextField
+                name="options"
+                className="question"
+                label="options"
+                placeholder="Options sepereated by ; (e.g. a; b; c; d)"
+                value={editPanel.state.question_array[index].options}
+                onChange={e => this.handleTextInput(editPanel, index, e)}
+                fullWidth
+                multiline
+              />
+            </Grid>
+          ) : (
+            <div></div>
+          )}
+          <Grid item xs={2}>
+            <IconButton
+              aria-label="delete"
+              onClick={e => this.removeQuestion(editPanel, index, e)}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Survey;
