5f438a860294f0c442226c878b07829ee8042ab6
aiwen324 <aiwen324@gmail.com>
2020-04-05 06:21:37 -0400

    Fix the incorrect merge

--- src/actions/user.js
+++ src/actions/user.js
@@ -108,7 +108,6 @@ export const submitRegister = (eventComp, currentUser) => {
   });
 };
 
-<<<<<<< HEAD
 // Helper function for sending the comments
 export const post_comment = (eventComp) => {
   const { announcement, input_comment } = eventComp.state;
@@ -189,8 +188,4 @@ export const fetchComments = (announcement) => {
       return res.comments;
     });
   // .catch((error) => console.log(error));
-=======
-export const validateSurvey = (questionMap) => {
-  let flag = true;
->>>>>>> 09e6cad336f528d41b708cf3aead22b85de5508b
 };
